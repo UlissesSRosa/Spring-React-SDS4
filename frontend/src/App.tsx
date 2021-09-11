a6b3cb8aa79f2a7b29c5920dfa953ca01da019c1
Ulisses Rosa <ulisses.skysect@hotmail.com>
2021-09-11 14:18:39 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,8 @@
-import BarChart from 'components/BarChart';
-import Datatable from 'components/Datatable';
-import DonutChart from 'components/DonutChart';
-import Footer from 'components/Footer';
-import NavBar from 'components/NavBar';
-import React from 'react';
+import Routes from "Routes"
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">DashBoard</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas Vendas(por colaborador)</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div>
-          <h2 className="text-primary">Todas Vendas</h2>
-        </div>
-        <Datatable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
